3c17122e1f46e1c4810300a1c9400f3f67a688bb
RuiOkazaki <okazaki200226@gmail.com>
2022-06-01 22:55:22 +0900

    feat: linkとして機能するように修正、hover時に不透明度を調整

--- src/components/ui-parts/Article/index.tsx
+++ src/components/ui-parts/Article/index.tsx
@@ -1,43 +1,52 @@
 import { Box, Flex, Heading, Img } from '@chakra-ui/react';
-import React, { VFC } from 'react';
+import NextLink from 'next/link';
+import { VFC } from 'react';
 
 import CardAuthor from 'components/ui-elements/CardAuthor';
 import CardDate from 'components/ui-elements/CardDate';
 import { Blog } from 'types/blog';
 
 type Props = {
-  blogData: Pick<Blog, 'title' | 'thumbnail' | 'createdAt'>;
+  blogData: Pick<Blog, 'id' | 'title' | 'author' | 'thumbnail' | 'createdAt'>;
 };
 
 const Article: VFC<Props> = ({ blogData }) => (
-  <Box
-    bg="white"
-    w="47.5%"
-    height="fit-content"
-    textStyle="article"
-    borderRadius="8px"
-    overflow="hidden"
-  >
-    <Img
-      src={blogData.thumbnail.url}
-      objectFit="cover"
-      sx={{
-        aspectRatio: '541 / 272.11',
+  <NextLink href={`/blog/${blogData.id}`} passHref>
+    <Box
+      as="a"
+      bg="white"
+      w="47.5%"
+      height="fit-content"
+      textStyle="article"
+      borderRadius="8px"
+      overflow="hidden"
+      cursor="pointer"
+      transition="all 0.2s"
+      _hover={{
+        opacity: 0.7,
       }}
-    />
-    <Flex
-      justifyContent="space-between"
-      flexDirection="column"
-      gap="24px"
-      py="24px"
-      px="16px"
     >
-      <CardDate>{blogData.createdAt}</CardDate>
-      <Heading as="h2" height="72px">
-        {blogData.title}
-      </Heading>
-      <CardAuthor author="kinoshita" />
-    </Flex>
-  </Box>
+      <Img
+        src={blogData.thumbnail.url}
+        objectFit="cover"
+        sx={{
+          aspectRatio: '541 / 272.11',
+        }}
+      />
+      <Flex
+        justifyContent="space-between"
+        flexDirection="column"
+        gap="24px"
+        py="24px"
+        px="16px"
+      >
+        <CardDate>{blogData.createdAt}</CardDate>
+        <Heading as="h3" height="72px">
+          {blogData.title}
+        </Heading>
+        <CardAuthor author={blogData.author[0]} />
+      </Flex>
+    </Box>
+  </NextLink>
 );
 export default Article;
