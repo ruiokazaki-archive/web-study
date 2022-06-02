754be99bd26e641bbe7afb1b1ad146d367f83c8a
RuiOkazaki <okazaki200226@gmail.com>
2022-06-02 13:33:13 +0900

    feat: 先にコンテンツを公開する

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,39 @@
-import { Text } from '@chakra-ui/react';
-import { NextPage } from 'next';
+import { Box, Flex, Heading } from '@chakra-ui/react';
 
-const Index: NextPage = () => (
-  <Text textStyle="card.title">Next ❤️ Chakra</Text>
-);
+import Card from 'components/ui-parts/Card';
+import { getBlogs } from 'libs/apiClient';
+import { Blog } from 'types/blog';
 
-export default Index;
+import type { NextPage } from 'next';
+
+type Props = {
+  blogData: Blog[];
+};
+
+const index: NextPage<Props> = ({ blogData }) => {
+  // eslint-disable-next-line no-console
+  console.log(blogData);
+
+  return (
+    <Box width="1000px" mx="auto">
+      <Heading as="h1">ブログ一覧</Heading>
+      <Flex flexWrap="wrap" justifyContent="space-between" gap="40px">
+        {blogData.map((blog) => (
+          <Card key={blog.id} blogData={blog} />
+        ))}
+      </Flex>
+    </Box>
+  );
+};
+
+export const getStaticProps = async () => {
+  const microCMSBlogs = await getBlogs({ limit: 1000 });
+
+  return {
+    props: {
+      blogData: microCMSBlogs.contents,
+    },
+  };
+};
+
+export default index;
