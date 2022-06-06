573ea16563d573df8ea34e522ce4130d2bba5b62
manami-sato <manamisato2001@gmail.com>
2022-06-06 15:00:09 +0900

    chore: article tagの型定義を編集

--- src/types/tag.ts
+++ src/types/tag.ts
@@ -1,6 +1,11 @@
 import { MicroCMSObject } from 'types/microCMS';
 
 export type Tag = MicroCMSObject & {
+  createdAt: string;
+  id: string;
   nameEn: string;
   nameJa: string;
+  publishedAt: string;
+  revisedAt: string;
+  updatedAt: string;
 };
