369f2760e8d33d01a709b70c7a53af231bc4fc9c
manami-sato <manamisato2001@gmail.com>
2022-06-09 12:54:43 +0900

    feat: article下部のトップに戻るリンクの作成

---
+++ src/components/ui-elements/ArticleIndexLink/index.tsx
@@ -0,0 +1,24 @@
+import { Flex, Text } from '@chakra-ui/react';
+import { VFC } from 'react';
+
+import IndexIcon from '../../../assets/IndexIcon';
+
+const ArticleListLink: VFC = () => (
+  <Flex
+    as="a"
+    href="/"
+    gap="8px"
+    w="fit-content"
+    m="0 auto"
+    opacity="1"
+    transition="opacity 0.2s"
+    _hover={{
+      opacity: '0.6',
+    }}
+  >
+    <IndexIcon />
+    <Text fontWeight="bold">トップページへ戻る</Text>
+  </Flex>
+);
+
+export default ArticleListLink;
