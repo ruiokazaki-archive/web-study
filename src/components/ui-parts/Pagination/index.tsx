e1603da6685bd297f9dafac8cc4e790dc9f15ccf
RuiOkazaki <okazaki200226@gmail.com>
2022-06-10 09:45:16 +0900

    feat: 正しいページに遷移するようにした

--- src/components/ui-parts/Pagination/index.tsx
+++ src/components/ui-parts/Pagination/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex } from '@chakra-ui/react';
 import NextLink from 'next/link';
+import { useRouter } from 'next/router';
 import { ReactNode, VFC } from 'react';
 
 type CircleProps = {
@@ -12,23 +13,28 @@ const Circle: VFC<CircleProps> = ({
   pageNumber,
   currentPage = false,
   children,
-}) => (
-  <NextLink href={`/${pageNumber}`}>
-    <Box
-      w="48px"
-      h="48px"
-      cursor="pointer"
-      borderRadius="full"
-      textAlign="center"
-      lineHeight="48px"
-      bg={currentPage ? 'main' : 'transparent'}
-      color={currentPage ? 'white' : 'text'}
-      textStyle="pagination"
-    >
-      {children}
-    </Box>
-  </NextLink>
-);
+}) => {
+  const router = useRouter();
+  const pathCategory = router.query.category as string;
+
+  return (
+    <NextLink href={`${pathCategory}/${pageNumber}`}>
+      <Box
+        w="48px"
+        h="48px"
+        cursor="pointer"
+        borderRadius="full"
+        textAlign="center"
+        lineHeight="48px"
+        bg={currentPage ? 'main' : 'transparent'}
+        color={currentPage ? 'white' : 'text'}
+        textStyle="pagination"
+      >
+        {children}
+      </Box>
+    </NextLink>
+  );
+};
 
 type Props = {
   totalBlogCount: number;
