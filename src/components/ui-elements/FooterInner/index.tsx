e0183cf7c4765b0cc19ae7935da25f89e903d6bb
ryou-yattung <92421273+ryou-yattung@users.noreply.github.com>
2022-06-10 11:17:46 +0900

    feat: footerを作成した

---
+++ src/components/ui-elements/FooterInner/index.tsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react';
+import { VFC } from 'react';
+
+const FooterInner: VFC = () => (
+  <Box textAlign="center" fontWeight="bold">
+    © 2022 うぇぶスタ All Rights Reserved.
+  </Box>
+);
+export default FooterInner;
---
+++ src/components/ui-elements/Tag/index.tsx
@@ -0,0 +1,23 @@
+import { Button } from '@chakra-ui/react';
+import Link from 'next/link';
+import { VFC } from 'react';
+
+type Props = { tag: string; tagUrl: string };
+
+const Tag: VFC<Props> = ({ tag, tagUrl }) => (
+  <Link href={tagUrl}>
+    <Button
+      colorScheme="gray"
+      variant="outline"
+      px="10px"
+      mx="8px"
+      my="10px"
+      borderRadius="20px"
+      fontSize="12px"
+      _hover={{ opacity: '0.5' }}
+    >
+      {tag}
+    </Button>
+  </Link>
+);
+export default Tag;
