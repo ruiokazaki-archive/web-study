e260b219c5cfb1929b734f21dde85dc571239e11
OkazakiRui <okazaki200226@gmail.com>
2022-06-29 16:24:43 +0900

    fix: next-sitemapの設定を変更した

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
   siteUrl: 'https://web-study.blog',
+  outDir: './out',
   generateRobotsTxt: true,
+  sitemapSize: 7000,
 };
 
 module.exports = config;
